cc27cb5b82f76b83669553c32f4df57ce9baf300
Xgor <real.xgor@hotmail.com>
2026-05-20 11:55:48 +0200

    End of Lecture

--- Program.cs
+++ Program.cs
@@ -4,10 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
+        int a = 1;
+        int b = 2;
+        Sum(a, b);
+     //   int c = a + b;
+     //   Console.WriteLine(c);
+        int[] numbers = Enumerable.Range(1, 100).ToArray();
+        foreach (int number in numbers)
+        {
+            int x = number;
+            Console.WriteLine(x);
+        }
+    }
+
+    private static void Sum(int a, int b)
+    {
+        Console.WriteLine(a + b);
+        
     }
 }
